7b274bbc88e7525d13c8adc5cd347cc1e2e0b129
Samuel Powell <samuelpowell@users.noreply.github.com>
2017-12-29 20:26:49 +0000

    WIP

--- src/CUFFT.jl
+++ src/CUFFT.jl
@@ -74,8 +74,6 @@ function plan(dest::CuArray, src::CuArray; compat::Symbol = :padding, stream=CuD
     sz = plan_size(dest, src)
     inembed = reverse(Cint[size(src)...])
     onembed = reverse(Cint[size(dest)...])
-    inembed[end] = 1
-    onembed[end] = 1
     plantype = plan_dict[(eltype(src),eltype(dest))]
     lib.cufftPlanMany(p, ndims(dest), sz, inembed, 1, 1, onembed, 1, 1, plantype, 1)
     pl = Plan{eltype(src),eltype(dest),ndims(dest)}(p[1])
